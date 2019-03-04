7b9f6e54967e7e56ee0f3026870fc9a0e429da83
Anton <antonoutkine@gmail.com>
2019-03-04 06:55:50 -0600

    Fix some bugs

--- nand2tetris/06/parser/parser.py
+++ nand2tetris/06/parser/parser.py
@@ -1,59 +1,81 @@
 import re
+import argparse
 
-predefined = {'SP': 0, 'LCL': 1, 'ARG': 2, 'THIS': 3, 'THAT': 4, 'SCREEN': 0x4000, 'KBD': 0x6000}
-cs = {'0': '101010', '1': '111111', '-1': '111010', 'D': '001100', 'A': '110000', '!D': '001101', '!A': '110001', '-D': '001111', '-A': '110011', 'D+1': '011111', '1+D': '011111', 'A+1': '110111', '1+A': '110111', 'D-1': '001110', 'A-1': '110010', 'D+A': '000010', 'A+D': '000010', 'D-A': '010011', 'A-D': '000111', 'D&A': '000000', 'A&D': '000000', 'D|A': '010101', 'A|D': '010101'}
+parser = argparse.ArgumentParser(description='Translate assembly code to hack.')
+parser.add_argument('infile', type=argparse.FileType('r'))
+parser.add_argument('outfile', nargs='?', type=str)
+args = parser.parse_args()
+lines = [line.strip() for line in args.infile.readlines()]
+
+predefined = {
+    'SP': 0, 'LCL': 1, 'ARG': 2, 'THIS': 3, 'THAT': 4, 'SCREEN': 0x4000, 'KBD': 0x6000,
+    **{f'R{i}': i for i in range(16)}
+}
+
+ds = {'AMD': '111', 'AD': '110', 'AM': '101', 'A': '100', 'MD': '011', 'D': '010', 'M': '001'}
+cs = {'A|D': '010101', 'D|A': '010101', 'A&D': '000000', 'D&A': '000000', 'A-D': '000111', 'D-A': '010011', 'A+D': '000010', 'D+A': '000010', 'A-1': '110010', 'D-1': '001110', '1+A': '110111', 'A+1': '110111', '1+D': '011111', 'D+1': '011111', '-A': '110011', '-D': '001111', '!A': '110001', '!D': '001101', 'A': '110000', 'D': '001100', '-1': '111010', '1': '111111', '0': '101010'}
+cs = {**{key.replace('A', 'M'): val for key, val in cs.items() if 'A' in key}, **cs}
 js = {'JGT': '001', 'JEQ': '010', 'JGE': '011', 'JLT': '100', 'JNE': '101', 'JLE': '110', 'JMP': '111'}
-ds = {'M': '001', 'D': '010', 'MD': '011', 'A': '100', 'AM': '101', 'AD': '110', 'AMD': '111'}
 
 symbol = r'(\.|\$|:|[a-zA-Z])(\.|\$|:|\w)*'
-syntax = {
+comment = r'\s*\/\/.*'
+
+def joinre(mappings):
+    return '|'.join(map(re.escape, mappings.keys()))
+
+syntax = {k: v + rf'({comment})?' for k, v in {
     'label': rf'\((?P<label>{symbol})\)',
-    'comment': r'\/\/.*',
-    'a_instruction': rf'@((?P<name>{symbol})|(?P<value>\d+))$',
-    'c_instruction': r'((?P<d>.+)=)?(?P<c>(.+(?=;)|.+))(;(?P<j>.+))?',
-    'newline': '\n'
-}
+    'a_instruction': rf'@((?P<name>{symbol})|(?P<value>\d+))',
+    'c_instruction': rf'((?P<d>{joinre(ds)})=)?(?P<c>({joinre(cs)}(?=;)|{joinre(cs)}))(;(?P<j>{joinre(js)}))?',
+    'newline': ''
+}.items()}
 
 symbols = {}
 hack = []
 variable_address = 0x400
 
-def toBinary(num):
-    return format(num, '015b')
+def numToInstruction(num):
+    return '0' + format(num, '015b')
 
+def error(exception):
+    print('Error compiling: ' + exception)
+    exit(1)
 
-for line in open('./program.asm'):
+line_number = 0
+for line in lines:
+    res = re.match(syntax['label'], line)
+    if res:
+        if res.group('label') in symbols:
+            error(f'label "{res.group("label")}" already defined')
+        symbols[res.group('label')] = line_number
+
+    elif re.match(syntax['a_instruction'] + '|' + syntax['c_instruction'], line):
+        line_number += 1
+
+for line in lines:
     res = re.match('|'.join(syntax.values()), line)
     if not res:
-        raise Exception('Invalid syntax')
-
-    if res.group('label'):
-        if res.group('label') in symbols:
-            raise Exception('Label already defined')
-        symbols[res.group('label')] = len(hack)
+        error(f'invalid syntax: "{line}"')
 
     elif res.group('name'):
         if res.group('name') not in predefined:
             if res.group('name') not in symbols:
+                print(res.group('name'))
                 symbols[res.group('name')] = variable_address
                 variable_address += 1
             name = symbols[res.group('name')]
         else:
             name = predefined[res.group('name')]
-        hack.append('0' + toBinary(name))
+        hack.append(numToInstruction(name))
 
     elif res.group('value'):
-        hack.append('0' + toBinary(int(res.group('value'))))
-
+        hack.append(numToInstruction(int(res.group('value'))))
 
     elif res.group('c'):
         a = '1' if 'M' in res.group('c') else '0'
+        hack.append('111' + a + cs[res.group('c')] + ds.get(res.group('d'), '000') + js.get(res.group('j'), '000'))
 
-        hack.append(
-            '111' + a + cs[res.group('c').replace('M', 'A')]
-            + ds.get(res.group('d'), '000') + js.get(res.group('j'), '000')
-        )
-
-with open('./out.hack', 'w') as write_file:
-    write_file.writelines('\n'.join(hack))
+outfile = args.outfile or args.infile.split['.'][0] + '.hack'
+with open(outfile, 'w') as write_file:
+    write_file.writelines('\n'.join(hack) + '\n')
 
