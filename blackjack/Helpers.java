a10b87e2863aba47d3fa1efa1296b42ce10ca943
Anton <antonoutkine@gmail.com>
2019-03-25 13:52:47 -0500

    Add javadoc to helpers

--- blackjack/Helpers.java
+++ blackjack/Helpers.java
@@ -2,6 +2,12 @@ import java.util.Scanner;
 import java.util.List;
 
 public class Helpers {
+    /**
+     * Confirms a prompt by asking the player for a "Y" or "N"
+     * @param scan the scanner to use
+     * @param prompt the prompt to display
+     * @return whether the player confirmed
+     */
     public static boolean confirm(Scanner scan, String prompt) {
         System.out.println("\n" + prompt + " Y/N");
 
@@ -14,6 +20,13 @@ public class Helpers {
         return input.equals("Y");
     }
 
+    /**
+     * Confirms a prompt by asking the player for one of a list of options
+     * @param scan the scanner to use
+     * @param prompt the prompt to display
+     * @param options the list of options to use
+     * @return the option confirmed
+     */
     public static String confirmOptions(Scanner scan, String prompt, List<String> options) {
         System.out.println("\n" + prompt + " " + String.join("/", options));
         String input = scan.next();
@@ -30,6 +43,11 @@ public class Helpers {
         }
     }
 
+    /**
+     * Get an integer from the player
+     * @param scan the scanner to use
+     * @return the integer
+     */
     public static int getInt(Scanner scan) {
         int result = 0;
         boolean done = false;
